294d97ce2bf33c90eaf740a4ac707d0c9cd6be09
Alexey <kozlovalex2005@gmail.com>
2014-06-08 14:22:39 +0300

    Remove unused projects.

--- Indigo.Hash/HashAlgorithms.h
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-typedef unsigned long long DIGEST_TYPE;
-
-DIGEST_TYPE __declspec(dllexport) GoulburnHash(char *cp, unsigned int len);
